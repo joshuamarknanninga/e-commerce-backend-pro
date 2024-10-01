7bd9c0b5e3d8f2f24b8213e5591640931f100194
Joshua Nanninga <jmarknanninga@gmail.com>
2024-10-01 12:18:15 -0500

    figuring out i need to npm run seeds

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
     port: process.env.DB_PORT || 5432,
-    login
+    logging: false,
   }
 );
 
